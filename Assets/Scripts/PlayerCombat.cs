959baa28f0cdbfbf0cf45b31f9e79803ed46de86
Mehmetcan Kılınç <mehmtcankilinc@hotmail.com>
2023-12-15 15:31:20 +0300

    player

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerCombat : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private LayerMask enemyLayers;
+    [SerializeField] private float attackDamage;
+    [SerializeField] private float attackCoolDown;
+    [SerializeField] private float attackRange;
+    private bool canAttack = true;
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed && canAttack)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+    IEnumerator Attack()
+    {
+        if (canAttack)
+        {
+            canAttack = false;
+            anim.SetTrigger("attack");
+            yield return new WaitForSeconds(0.7f);
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayers);
+            foreach(Collider2D hit in hitEnemies)
+            {
+                if (Vector2.Distance(hit.transform.position, transform.position) < attackRange)
+                {
+                    //hit.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+                }
+            }
+            yield return new WaitForSeconds(attackCoolDown);
+            canAttack = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CapsuleCollider2D capsuleCollider;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float moveSpeed = 7.5f;
+    [SerializeField] private float jumpingForce = 15f;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    [SerializeField] private float dashCooldown;
+    private bool isFacingRight = true;
+    private bool isDashing = false;
+    private bool canDash = true;
+    private float horizontal;
+    private enum MovementState {idle, run, jump, fall}
+    void Update()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        if (!isFacingRight && horizontal > 0f)
+            Flip();
+        else if (isFacingRight && horizontal < 0f)
+            Flip();
+
+        Animations();
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 0.25f;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1f;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+    void Animations()
+    {
+        MovementState state;
+
+        if (!IsGrounded())
+        {
+            if (rb.velocity.y > 0f)
+                state = MovementState.jump;
+            else
+                state = MovementState.fall;
+        }
+        else
+        {
+            if (horizontal != 0f)
+                state = MovementState.run;
+            else
+                state = MovementState.idle;
+        }
+        anim.SetInteger("state", (int)state);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+            rb.velocity = new Vector2(rb.velocity.x, jumpingForce);
+    }
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.performed && canDash)
+            StartCoroutine(Dash());
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.4f, groundLayer);
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        anim.SetTrigger("dash");
+        rb.velocity = new Vector2(horizontal * dashSpeed, rb.velocity.y);
+        
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+        
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+    public IEnumerator SetCollider()
+    {
+        capsuleCollider.offset = new Vector2(0.008911133f, -1.32495f);
+        capsuleCollider.size = new Vector2(1.264221f, 1.620093f);
+        yield return new WaitForSeconds(0.25f);
+        capsuleCollider.offset = new Vector2(0.008911133f, -0.06704926f);
+        capsuleCollider.size = new Vector2(1.264221f, 4.135895f);
+    }
+}
