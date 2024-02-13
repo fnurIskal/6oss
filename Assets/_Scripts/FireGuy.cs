c6806ae9afa8c0c2d417ffe2d797b227ce3cacd0
begumaygun <aygunbegum02@gmail.com>
2024-02-13 16:10:49 +0300

    fireguy
    daha bitmedi

--- Assets/_Scripts/FireGuy.cs
+++ Assets/_Scripts/FireGuy.cs
@@ -16,6 +16,9 @@ public class FireGuy : MonoBehaviour
     private Animator anim;
     public float health;
     public float maxHealth=10f;
+    private Rigidbody2D rb;
+    private bool isPlayerAttacking = false;
+   
    
     enum MovementState { idle,attack,hurt,death}
     
@@ -24,6 +27,9 @@ public class FireGuy : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player").transform;
         anim = GetComponent<Animator>();
         health = maxHealth;
+        rb= GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        
 
     }
 
@@ -31,7 +37,19 @@ public class FireGuy : MonoBehaviour
     void Update()
     {
         MovementState state;
-       
+
+        // Oyuncunun saldýrý durumu kontrol ediliyor
+        if (isPlayerAttacking)
+        {
+            // Düþmanýn hasar almasý
+            EnemyTakeDamage(1); // Hasar miktarý burada 1 olarak varsayýlmýþtýr, deðiþtirebilirsiniz
+        }
+
+        if (Player.position.x < transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+
         if (Vector2.Distance(transform.position,Player.position) < retreatDistance)
         {
             float step = -speed * Time.deltaTime;
@@ -39,6 +57,9 @@ public class FireGuy : MonoBehaviour
             transform.position = new Vector3(newPosition.x, transform.position.y, transform.position.z);
             state = MovementState.idle;
             anim.SetInteger("state", (int)state);
+
+            
+
         }
         else if (Vector2.Distance(transform.position, Player.position) > shootingRange)
         {
@@ -76,21 +97,30 @@ public class FireGuy : MonoBehaviour
     }
     public void EnemyTakeDamage(int amount)
     {
+        Debug.Log("!");
         health -= amount;
         if(health<=0)
         {
-            anim.SetTrigger("death");
-            Invoke("RestartGame", 1f);
+            die();
         }
         else
         {
             anim.SetTrigger("hurt");
         }
     }
+
+    void die()
+    {
+        anim.SetTrigger("death");
+        rb.isKinematic = false;
+        Invoke("RestartGame", 1f);
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+  
 
 
 }
